0211c96cb71025a19810492acb53a9b47284e73d
Taylor A. Liss <tayloraliss@gmail.com>
2017-07-13 11:49:29 -0400

    first commit

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,38 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> inside default constructor");
+	}
+	
+	// define a setter method for injections
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> inside setFortuneService() method");
+		fortuneService = theFortuneService;
+	}
+	
+//	@Autowired //spring will scan for a component that implements the FortuneService interface
+//	public TennisCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
